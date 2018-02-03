cc6be6b8857ced99ad463d3097ff1243072dd770
Doug Kent <github@douglaskent.com>
2018-02-03 11:11:47 -0500

    spiffing up the readme

--- package-scripts.js
+++ package-scripts.js
@@ -32,8 +32,8 @@ const pathDaostackArcGanacheDbZip = joinPath(pathArcJsRoot, "ganacheDb.zip");
 const network = env.network || config.get('network');
 
 // this is needed to force travis to use our modified version of truffle
-const truffleIsInternal = fs.existsSync(joinPath("node_modules", "truffle-core-migrate-without-compile"));
-const truffleCommand = `node ${joinPath(truffleIsInternal ? "node_modules" : "../", "truffle-core-migrate-without-compile", "cli")}`;
+const truffleIsInternal = fs.existsSync(joinPath(pathArcJsRoot, "node_modules", "truffle-core-migrate-without-compile"));
+const truffleCommand = `node ${joinPath(pathArcJsRoot, truffleIsInternal ? "node_modules" : "../", "truffle-core-migrate-without-compile", "cli")}`;
 
 module.exports = {
   scripts: {
@@ -151,11 +151,16 @@ module.exports = {
        */
       clean: {
         default: rimraf(joinPath(pathArcJsContracts, "*")),
+        /**
+         * clean, fetch and migrate.
+         * Run this ONLY when you want to start with fresh UNMIGRATED contracts from daostack-arc.
+         */
         andMigrate: series("nps migrateContracts.clean", "nps migrateContracts.fetchFromArc", "nps migrateContracts")
       },
       /**
        * Fetch the unmigrated contract json files from DAOstack-Arc.
-       * Run this ONLY when we want to start with fresh unmigrated contracts from daostack-arc.
+       * Run this ONLY when you want to start with fresh UNMIGRATED contracts from daostack-arc.
+       * Best to run "migrateContracts.clean" first.
        */
       fetchFromArc: copy(`${joinPath(pathDaostackArcRepo, "build", "contracts", "*")}  ${pathArcJsContracts}`)
     }
